e2f8148ebc976044aa2e42b90ca92cb6edb7f96f
Tom Boutell <tom@punkave.com>
2017-09-05 13:21:11 -0400

    don't forget the modules key

--- local.example.js
+++ local.example.js
@@ -4,16 +4,18 @@
 // deployments in our stagecoach recipe
 
 module.exports = {
-  'apostrophe-assets': {
-    // Set to true for full CSS and JS minify, on staging and production servers
-    // minify: true
-  },
-  // If these are your db settings then you don't need to be explicit. If not
-  // you can uncomment this and get more specific.
-  'apostrophe-db': {
-    // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
-    // There is legacy support for host, port, name, user and password options,
-    // but this is not necessary. They can all go in the uri option like this:
-    // mongodb://user:password@host:port/dbname
+  modules: {
+    'apostrophe-assets': {
+      // Set to true for full CSS and JS minify, on staging and production servers
+      // minify: true
+    },
+    // If these are your db settings then you don't need to be explicit. If not
+    // you can uncomment this and get more specific.
+    'apostrophe-db': {
+      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
+      // There is legacy support for host, port, name, user and password options,
+      // but this is not necessary. They can all go in the uri option like this:
+      // mongodb://user:password@host:port/dbname
+    }
   }
 };
