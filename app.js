fad22945216a2a00917deb5a981f6b60bc5a657e
Tom Boutell <tom@punkave.com>
2017-11-03 10:23:33 -0400

    Use a fallback views/ folder, avoid unnecessary cross module paths, removed unused title option

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+var path = require('path');
+
 var apos = require('apostrophe')({
   shortName: 'apostrophe-boilerplate',
-  title: 'Apostrophe Boilerplate',
 
   // See lib/modules for basic project-level configuration of our modules
   // responsible for serving static assets, managing page templates and
   // configuring user acounts.
 
   modules: {
-    // Add custom apostrophe-modules and their respective configuration here!
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+
   }
 });
